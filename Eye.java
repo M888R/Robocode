95bdc4e2d7bedb29c5a93a1d8cae46e44e7f914e
Manas <manas@shahparivar.info>
2017-09-20 08:04:56 -0700

    Add enemyArray {unstable}

--- Eye.java
+++ Eye.java
@@ -8,42 +8,56 @@ import java.awt.geom.Point2D;
 
 /**
  * Eye - a robot by Manas Shah
+ * Current state: predictive targeting is disabled.
+ * TODO: Add loop through generated 2D array of data strings and update it for every robot to track them all
  */
 public class Eye extends AdvancedRobot
 {
-        AdvancedEnemyBot enemy = new AdvancedEnemyBot();
-	
-        
-        String target = "";
-        private int gunTurnError = 5;
-        private int maxBulletMiss = 7;
+    AdvancedEnemyBot enemy = new AdvancedEnemyBot();
+
+    
+    String target = "";
+    private int gunTurnError = 5;
+    private int maxBulletMiss = 7;
+    private int sideToggle = 1;
+    private String[][] enemyArray;
+    private double currentEnergy = 100.0;
+    private String hitBy = "";
+    private int missCount = 0;
+    private boolean usePredict = false;
 
 	/**
 	 * dodgeBullet: Dodge a bullet when it's fired at you.
 	 */
-    int sideToggle = 1;
     public void dodgeBullet(AdvancedEnemyBot event) {
 		// use 'event' object for onScannedRobotEvent and dodge the bullet.
 	        setTurnRight(event.getBearing() - 90 + (10 * sideToggle));
                 setAhead(50 * sideToggle);
                 sideToggle *= -1;
 	}
+    public void updateEnemyArray(ScannedRobotEvent event) {
         /**
-         * isBulletFired: Check if bullet is fired.
-         * Returns: True if bullet fired, False if not.
-         */
-        double currentEnergy = 100.0;
-        public boolean isBulletFired(AdvancedEnemyBot scanEvent) {
-                double prevEnergy = currentEnergy; // this basically doesn't work, you need to track individual robots.
-                currentEnergy = scanEvent.getEnergy();
-                double scanDifference = prevEnergy - currentEnergy;
-                if((prevEnergy > currentEnergy) && (scanDifference <= 3.0)) {
-                        return true;
-                }
-                else {
-                        return false;
-                }
+         * loop through enemy array and see if e.getName() is equal to the 0 slot of any of the arrays in it.
+         * If it is, set the array at that index with 0 = getName, 1 = what 2 was before, and 2 = getEnergy().
+         * If it's not, append an array with 0 = getName, 1 = getEnergy, 2 = getEnergy.
+         * Once that is done, call it with reference to e inside onScannedRobot and then make a new function to handle it for isBulletFired
+        */
+    }
+
+    /**
+     * isBulletFired: Check if bullet is fired.
+     * Returns: True if bullet fired, False if not.
+     */
+    public boolean isBulletFired(AdvancedEnemyBot scanEvent) {
+        double prevEnergy = currentEnergy; // this basically doesn't work, you need to track individual robots.
+        currentEnergy = scanEvent.getEnergy();
+        double scanDifference = prevEnergy - currentEnergy;
+        if((prevEnergy > currentEnergy) && (scanDifference <= 3.0)) {
+                return true;
+        } else {
+                return false;
         }
+    }
 	/**
 	 * run: Eye's default behavior
 	 */
@@ -74,12 +88,12 @@ public class Eye extends AdvancedRobot
 	public void onScannedRobot(ScannedRobotEvent e) {
 		String currentScanned = e.getName();
 		//target = (target == "") ? currentScanned : target;
-                if (enemy.none() || e.getName().equals(enemy.getName()) || e.getDistance() < enemy.getDistance() - 50 || e.getName().equals(hitBy)) {
-                        enemy.update(e, this);
-                }
-                if (isBulletFired(enemy)) {
-        	        dodgeBullet(enemy);
-                }
+        if (enemy.none() || e.getName().equals(enemy.getName()) || e.getDistance() < enemy.getDistance() - 50 || e.getName().equals(hitBy)) {
+                enemy.update(e, this);
+        }
+        if (isBulletFired(enemy)) {
+            dodgeBullet(enemy);
+        }
 		//if (target != currentScanned) {
                 //        execute();
                 //        return;
@@ -94,24 +108,23 @@ public class Eye extends AdvancedRobot
 
 	public void onRobotDeath(RobotDeathEvent deadBot) {
 		//target = (deadBot.getName() == target) ? "" : target;
-                if (deadBot.getName().equals(enemy.getName())) {
-                        enemy.reset();
-                }
+        if (deadBot.getName().equals(enemy.getName())) {
+                enemy.reset();
+        }
 
 	}
 
 	/**
 	 * onHitByBullet: What to do when you're hit by a bullet
 	 */
-        private String hitBy = "";
 	public void onHitByBullet(HitByBulletEvent e) {
 		// Replace the next line with any behavior you would like
 		//back(10);
-                hitBy = e.getName(); 
-                sideToggle *= -1;
-                setFire(1.1);
-                dodgeBullet(enemy); //augment later with enemyBot and input from the class
-                execute();
+        hitBy = e.getName(); 
+        sideToggle *= -1;
+        setFire(1.1);
+        dodgeBullet(enemy); //augment later with enemyBot and input from the class
+        execute();
 
 	}
 	
@@ -122,9 +135,6 @@ public class Eye extends AdvancedRobot
 		// Replace the next line with any behavior you would like
 		//back(20);
 	}
-
-    private int missCount = 0;
-    private boolean usePredict = false;
 
     public void onBulletMiss(BulletMissedEvent event) {
        // missCount++;
@@ -145,15 +155,16 @@ public class Eye extends AdvancedRobot
     public void checkFire(double firePower) {
         if(getGunHeat() == 0 && Math.abs(getGunTurnRemaining()) <= gunTurnError) setFire(firePower);
     }
-        public double normalizeBearing(double angle) {
-                while(angle > 180) angle -= 360;
-                while(angle < -180) angle += 360;
-                return angle;
-        }
 
-        public void moveXY(double x, double y) {
-                // and then there was trig
-        }
+    public double normalizeBearing(double angle) {
+            while(angle > 180) angle -= 360;
+            while(angle < -180) angle += 360;
+            return angle;
+    }
+
+    public void moveXY(double x, double y) {
+            // and then there was trig
+    }
 
     public long calcTime(AdvancedEnemyBot enemy) {
         // calculate firepower based on distance
